bf23380accbab00e769d70362e8f8dc38b81690f
archeq <piptem@gmail.com>
2026-02-01 20:07:20 +0100

    updated ViralDataset to load multiple CSV files and merge data; added error handling for CSV reading

--- dataset.py
+++ dataset.py
@@ -3,20 +3,43 @@ import pandas as pd
 from PIL import Image
 from torch.utils.data import Dataset
 import torch
+import glob
 
 
 class ViralDataset(Dataset):
-    def __init__(self, csv_file, img_dir, tokenizer, transform=None):
+    def __init__(self, csv_dir, img_dir, tokenizer, transform=None):
         # 1. Load CSV data
-        self.data = pd.read_csv(csv_file)
         self.img_dir = img_dir
         self.transform = transform
         self.tokenizer = tokenizer
 
+        csv_files = glob.glob(os.path.join(csv_dir, "*.csv"))
+
+        if not csv_files:
+            raise RuntimeError(f"No channel info found in: {csv_dir}")
+
+        print(f"Found {len(csv_files)} channel CSV-s. Merging data...")
+
+        # Merging into one DataFrame
+        df_list = []
+        for filename in csv_files:
+            try:
+                df = pd.read_csv(filename)
+                df_list.append(df)
+            except Exception as e:
+                print(f"Error reading {filename}: {e}")
+
+        self.data = pd.concat(df_list, ignore_index=True)
+
+
+
         # 2. Filter mid-range v-scores (0.8 to 1.5)
+        initial_len = len(self.data)
         self.data = self.data[
             (self.data['V-Score'] > 1.5) | (self.data['V-Score'] < 0.8)
             ].reset_index(drop=True)
+
+        print(f"Data is loaded. {len(self.data)} videos left after filtering (out of {initial_len}).")
 
         # 3. If v-score is >1.5, label as 1 (viral), else 0 (non-viral)
         self.data['label'] = (self.data['V-Score'] > 1.5).astype(int)
@@ -37,7 +60,7 @@ class ViralDataset(Dataset):
         try:
             image = Image.open(img_path).convert("RGB")
         except FileNotFoundError:
-            # Fallback if image was not found
+            # Fallback if image was not found - load black image instead
             image = Image.new('RGB', (224, 224), color='black')
 
         if self.transform:
