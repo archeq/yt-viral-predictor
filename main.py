b8ddbb63a238533fc4e392e291dcb2560127900f
archeq <piptem@gmail.com>
2026-02-02 02:16:28 +0100

    updated main.py to expand niche categories and streamline channel processing; added specific channel IDs for targeted data collection

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
     with open("api.txt", "r") as f:
         API_KEY = f.read().strip()
 
-    NICHES = ["programming", "software engineering", "vibe coding"]
+    NICHES = ["programming", "software engineering", "vibe coding", "counter strike", "study"]
 
     # Downloader initialization
     downloader = DataDownloader(
@@ -27,13 +27,26 @@ def main():
         OUTPUT_FOLDER="data/raw/"
     )
 
-    discovered_channels = downloader.channels_by_niche(NICHES, max_searches=10)
+    discovered_channels = downloader.channels_by_niche(NICHES, max_searches=3)
 
     if not discovered_channels:
         print("❌ ")
         return
 
-    chosen_channels = []  # If you want to include specific channels, add their IDs here
+    chosen_channels = [
+        'UCcgNiiErDGHICbNYtuyxW4A',
+        'UCV0qA-eDDICsRR9rPcnG7tw',
+        'UCPhyA52nHlW3L6r-7sDfcyg',
+        'UC7zZ2-Q_oxbUaoMVL0z51wg',
+        'UCG4pyCrLYfDsSaRUrBH1MKA',
+        'UCvYUyKg7wDj760PippmWhig',
+        'UCzSc8bhRKEKe7xEGp-5LWAg',
+        'UCZ59iKBmGRfQlnl73sOX0Lw',
+        'UCsguRUxh-0DiryIdPPWFeCQ',
+        'UCDlqbnftd2Z_Ysh8itMvhwg',
+        'UCMUsC4hvDRNefChiOJflyRg'
+                       ]
+    # If you want to include specific channels, add their IDs here
 
     print(f"\n📺 Final Collection: {len(discovered_channels)}  Unique Channels")
     print("-" * 30)
@@ -43,10 +56,17 @@ def main():
         print(f"\n--- Processing channel: {channel_title} ({channel_id}) ---")
 
         try:
-            # neighbor_count=10 is default value for Elastic Average
-            downloader.process_channel(channel_id, neighbor_count=10)
+            downloader.process_channel(channel_id)
         except Exception as e:
             print(f"❌ Error occurred while processing {channel_title}: {e}")
+
+    for channel_id in chosen_channels:
+        print(f"\n--- Processing channel: ({channel_id}) ---")
+
+        try:
+            downloader.process_channel(channel_id)
+        except Exception as e:
+            print(f"❌ Error occurred while processing {channel_id}: {e}")
 
     print("\n✅ Job finished successfully!")
 
